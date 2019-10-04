1d9d4cf9c7f4ed4dfdd382cd79e5df60a684221b
James Mobus <mobusj@live.com>
2019-10-04 17:15:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -78,7 +78,7 @@ class MusicLibraryController
   end
   
   def list_songs_by_genre()
-    puts "Please enter the name of an genre:"
+    puts "Please enter the name of a genre:"
     genre_name = gets
     genre = Genre.find_or_create_by_name(genre_name)
     
