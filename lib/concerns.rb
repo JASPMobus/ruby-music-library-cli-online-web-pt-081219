3de351c23a8ec5130df6d625d1c4ec82fe72e6db
James Mobus <mobusj@live.com>
2019-10-04 16:30:17 +0000

    Automatically backed up by Learn

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -1,5 +1,18 @@
 module Concerns
   module Findable
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name } 
+  end
+  
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    
+    if !song 
+      Song.create(name)
+    else
+      song 
+    end
+  end
     
   end
 end
