aeea2f3e182e92ffefa9e6680ece6b7f2dba5742
James Mobus <mobusj@live.com>
2019-10-04 16:32:11 +0000

    Automatically backed up by Learn

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -1,10 +1,10 @@
 module Concerns
   module Findable
-    def self.find_by_name(name)
+    def find_by_name(name)
       @@all.find { |instance| instance.name == name } 
     end
     
-    def self.find_or_create_by_name(name)
+    def find_or_create_by_name(name)
       instance = self.find_by_name(name)
       
       if !instance
