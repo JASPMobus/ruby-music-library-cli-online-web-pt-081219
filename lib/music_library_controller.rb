8a0c4718ce167e4ed7772c58ea1223c32ed5e56c
James Mobus <mobusj@live.com>
2019-10-04 17:06:27 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
   end
   
   def list_artists
-    alphabetized = Genre.all.sort { |genre1, genre2| genre1.name <=> genre2.name }
+    alphabetized = Artist.all.sort { |genre1, genre2| genre1.name <=> genre2.name }
     
     alphabetized..each_with_index do |artist, index|
       puts "#{index+1}. #{artist.name}"
