11ba3dc13ca68d065689d0db884073b27e55a8f2
James Mobus <mobusj@live.com>
2019-10-04 17:00:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -33,7 +33,7 @@ class MusicLibraryController
   
   def list_songs
     Song.all.each_with_index do |song, index| 
-      puts "#{index+1}. #{song.name} - #{song.artist.name} - #{song.genre.name}"
+      puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end 
   end
 end
