ed8b73fdd2403d67b0ddb26db8e8fd1f09c7780d
James Mobus <mobusj@live.com>
2019-10-04 16:46:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -3,5 +3,9 @@ class MusicLibraryController
   
   def initialize(path = "./db/mp3s")
     @path = path
+    
+    music_importer = MusicImporter.new(path)
+    
+    music_importer.import(path)
   end
 end
